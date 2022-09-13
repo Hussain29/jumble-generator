5246fe5a4fdf97aeb93465eeee648ec1baea0d87
Hussain Shaik <66374088+Hussain29@users.noreply.github.com>
2022-09-13 17:32:59 +0530

    Update jumble.py

--- jumble.py
+++ jumble.py
@@ -2,16 +2,16 @@
 import random    # using package random to use sample() function
 
 # creating variables to take input of 4 'clue' strings
-WORD1 = input("Enter the 1st word: ").upper()
-WORD2 = input("Enter the 2nd word: ").upper()
-WORD3 = input("Enter the 3rd word: ").upper()
-WORD4 = input("Enter the 4th word: ").upper()
+WORD1 = input('Enter the 1st word: ').upper()
+WORD2 = input('Enter the 2nd word: ').upper()
+WORD3 = input('Enter the 3rd word: ').upper()
+WORD4 = input('Enter the 4th word: ').upper()
 
 print('\n')
 
-# taking input of 'riddle' string
+# taking input of 'riddle' string adn hint
+HINT = input("Enter the hint phrase: ")
 RIDDLE = input("Enter the riddle word: ").upper()
-
 print('\n')
 
 # removing spaces from the 'riddle' string
@@ -27,9 +27,9 @@ REQ_LETTER = "[ _ ]"
 
 # checking if the 'riddle' is solvable from the given 'clues'
 if len(set(RIDWOSPACE) - set(MASTER)) == 0:
-    print("Riddle can be solved.", end = '')
+    print('Riddle can be solved.', end = '')
 else:
-    print("Riddle cannot be solved. Try Again.", end = '')
+    print('Riddle cannot be solved. Try Again.', end = '')
 
 print('\n')
 
@@ -39,7 +39,6 @@ check = list(RIDWOSPACE)
 #creating a list of the 4 'clue' words to iterate through
 words = [WORD1,WORD2,WORD3,WORD4]
 
-#print a seperating line
 print('+-' + '-----'*(len(RIDDLE)) + '-+')
 
 print('\n')
@@ -48,9 +47,9 @@ print('\n')
 for word in words:    #here we're going through the 'words'
     JUMBLE = ''.join(random.sample(word, len(word)))    #jumbles the current word in the loop 
     for ch in JUMBLE:
-        print("  "+ch+ "  ",end='')    #prints the letters from the jumble
+        print('  '+ ch + '  ',end='')    #prints the letters from the jumble
 
-    print("\n", end = '')    #now, we start printing blanks in the next line
+    print('\n', end = '')    #now, we start printing blanks in the next line
     for ch in word:
         if ch in check:    #this checks if the letter is required to solve the riddle
             print(REQ_LETTER,end='')    #prints the underlined blank
@@ -58,25 +57,27 @@ for word in words:    #here we're going through the 'words'
         else:
             print(BLANK,end='')    #prints regular blank
 
-    print("\n")
+    print('\n')
     JUMBLE = ''
 
-#print a seperating line
 print('\n')
 print('+-' + '-----'*(len(RIDDLE)) + '-+')
+print('\n')   
 
-print('\n')    
+print('The Riddle Phrase: ' + '"' + HINT + '"')
+print('\n')
+
 #add riddle word blanks
-print("Fill In The Riddle:")
+print('Fill In The Riddle:')
 print('\n')
 for ch in RIDDLE:    #prints the blanks for the 'riddle' word
     if ch == " ":
-        print("     ",end='')
+        print('     ',end='')
     else:
         print(BLANK,end='')
 
 print('\n')
-#print a seperating line
+
 print('\n')
 print('+-' + '-----'*(len(RIDDLE)) + '-+')
 print('\n')
